b314faefb1514a380f3b7abac2e2d8b3fbb89063
athoo <athoo@sina.com>
2015-04-04 22:27:33 +0800

    basic class function added

---
+++ rubygems_crawler.rb
@@ -0,0 +1,44 @@
+require 'gems'
+=begin
+first create a ~/.gem/credentials as below
+---
+:rubygems_api_key: XXXXXXX
+=end
+
+# get { gem_name:=> { version:=> { date:=>download_times } } }
+class Gems_downloads
+	
+	# initialize the class with the gem_name
+	def initialize(gem_name)
+		@gem_name = gem_name
+	end
+
+	# call the method to get what needed in a hash format
+	def get_versions
+		hash=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
+		vers = Gems.versions @gem_name
+		vers.each do |ver|
+			downloads=Gems.downloads @gem_name, ver["number"]
+			downloads.each do |k,v|
+				hash[@gem_name.to_s][ver["number"]][k] = v
+			end
+			# File.open("test", "a") do |aFile|
+			# 	result = @gem_name+ver["number"]+downloads.to_s
+			# 	aFile.syswrite(result)
+			# end
+		end
+		return hash
+	end
+
+end
+
+all_hash=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
+
+a=Gems_downloads.new "ltp_checker"
+
+hash=a.get_versions
+
+all_hash.merge!(hash)
+
+puts all_hash
+
