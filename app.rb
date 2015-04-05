80aa3017fd10805ec9c970afb5c685762bd1b11c
athoo <athoo@sina.com>
2015-04-05 20:16:27 +0800

    add exception rescue

--- app.rb
+++ app.rb
@@ -1,18 +1,25 @@
 require "./GemMiner.rb"
 require "ftools"
 
+# read the gem list
 gem_names=File.open("gem_list.txt").readlines.each do |x| x.strip! end
 
 
-all_dictionary=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
+
+# all_dictionary=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
 
 
-
+# crawling the downloads accumulated and save them in unique file
 gem_names[1..10].each do |gem_name| 
 	puts gem_name 
-	file=File.open("./downloads/"+gem_name+".txt","w")
-	gem_downloads = GemMiner.new gem_name
-	file.puts gem_downloads.get_versions_downloads_list
+	begin
+		file=File.open("./downloads/"+gem_name+".txt","w")
+		gem_downloads = GemMiner.new gem_name
+		file.puts gem_downloads.get_versions_downloads_list
+	rescue
+		file=File.open("error_gems.txt","a")
+		file.puts gem_name
+	end
 end
 
 
@@ -22,21 +29,3 @@ end
 # # hash=a.get_versions
 # puts a.get_yesterday_downloads
 
-
-# begin
-
-# rescue
-
-# end
-
-
-# all_hash.merge!(hash)
-
-# puts all_hash
-
-
-
-			# File.open("test", "a") do |aFile|
-			# 	result = @gem_name+ver["number"]+downloads.to_s
-			# 	aFile.syswrite(result)
-			# end
