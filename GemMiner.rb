a3ee22d0f31f27833e6cf9433574e3bde13a8256
athoo <athoo@sina.com>
2015-04-05 11:22:08 +0800

    remove trash

--- GemMiner.rb
+++ GemMiner.rb
@@ -32,13 +32,3 @@ class GemMiner < Sinatra::Base
 	end
 
 end
-
-all_hash=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
-
-a=Gems_downloads.new "ltp_checker"
-
-hash=a.get_versions
-
-all_hash.merge!(hash)
-
-puts all_hash
