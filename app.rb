f7fe1daff0a6ffb9e3009314749873a1935b109c
athoo <athoo@sina.com>
2015-04-05 20:00:10 +0800

    pass the testing for 10 gems crawling saved in downloads

--- app.rb
+++ app.rb
@@ -1,12 +1,34 @@
 require "./GemMiner.rb"
+require "ftools"
 
-all_hash=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
+gem_names=File.open("gem_list.txt").readlines.each do |x| x.strip! end
 
-a=GemMiner.new "movie_crawler"
-# puts a.get_vers_list
-puts a.get_versions_downloads_list
-# hash=a.get_versions
-puts a.get_yesterday_downloads
+
+all_dictionary=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
+
+
+
+gem_names[1..10].each do |gem_name| 
+	puts gem_name 
+	file=File.open("./downloads/"+gem_name+".txt","w")
+	gem_downloads = GemMiner.new gem_name
+	file.puts gem_downloads.get_versions_downloads_list
+end
+
+
+# a=GemMiner.new "movie_crawler"
+# # puts a.get_vers_list
+# puts a.get_versions_downloads_list
+# # hash=a.get_versions
+# puts a.get_yesterday_downloads
+
+
+# begin
+
+# rescue
+
+# end
+
 
 # all_hash.merge!(hash)
 
