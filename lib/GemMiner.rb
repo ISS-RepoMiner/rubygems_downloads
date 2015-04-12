600268b7864bffaeb4b542abee07b6694e3a2a07
Soumya Ray <soumya.ray@gmail.com>
2015-04-13 00:55:10 +0800

    Restart on dailyminer.rb script; tabs to two-spaces

--- lib/GemMiner.rb
+++ lib/GemMiner.rb
@@ -10,82 +10,82 @@ first create a ~/.gem/credentials as below
 # get { gem_name:=> { version:=> { date:=>download_times } } }
 class GemMiner
 
-	# initialize the class with the gem_name, input and output formulation
-	def initialize(gem_name)
-		@gem_name = gem_name
-		@hash=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
-		# @gem_info={}
-	end
+  # initialize the class with the gem_name, input and output formulation
+  def initialize(gem_name)
+    @gem_name = gem_name
+    @hash=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
+    # @gem_info={}
+  end
 
-	# scan all the versions of a gem
-	def get_vers_list
-		vers = Gems.versions @gem_name
-		vers_list={}
-		vers.each do |ver|
-			vers_list[ver["number"]]=ver["built_at"]
-		end
-		vers_list
-	end
+  # scan all the versions of a gem
+  def get_vers_list
+    vers = Gems.versions @gem_name
+    vers_list={}
+    vers.each do |ver|
+      vers_list[ver["number"]]=ver["built_at"]
+    end
+    vers_list
+  end
 
-	# check the downloads of a version series
-	def get_ver_downloads(ver)
-		Gems.downloads @gem_name, ver
-	end
+  # check the downloads of a version series
+  def get_ver_downloads(ver)
+    Gems.downloads @gem_name, ver
+  end
 
-	def get_ver_history_downloads_series(ver)
-		vers_list=get_vers_list
-		Gems.downloads @gem_name, ver, vers_list[ver],Date.today-1
-	end
+  def get_ver_history_downloads_series(ver)
+    vers_list=get_vers_list
+    Gems.downloads @gem_name, ver, vers_list[ver],Date.today-1
+  end
 
 
-	# check date downloads for specific version,date format "2014-12-05"
-	def get_ver_downloads_by_date(ver,date)
-		Gems.downloads @gem_name, ver, date,date
-	end
+  # check date downloads for specific version,date format "2014-12-05"
+  def get_ver_downloads_by_date(ver,date)
+    Gems.downloads @gem_name, ver, date,date
+  end
 
-	# get yesterday downloads of a specific version
-	def get_ver_yesterday_downloads (ver)
-		downloads = Gems.downloads @gem_name, ver, Date.today-1, Date.today-1
-	end
+  # get yesterday downloads of a specific version
+  def get_ver_yesterday_downloads (ver)
+    downloads = Gems.downloads @gem_name, ver, Date.today-1, Date.today-1
+  end
 
-	# save the downloads data to a structured hash_table
-	def save_to_hash(downloads,ver)
-		downloads.each do |k,v|
-			@hash[@gem_name][ver][k] = v
-		end
-	end
+  # save the downloads data to a structured hash_table
+  def save_to_hash(downloads,ver)
+    downloads.each do |k,v|
+      @hash[@gem_name][ver][k] = v
+    end
+  end
 
-	# call the method to get what needed in a hash format
-	def get_versions_downloads_list
-		vers_list = get_vers_list
+  # call the method to get what needed in a hash format
+  def get_versions_downloads_list
+    vers_list = get_vers_list
 
-		vers_list.each do |ver,built_at|
-			downloads = get_ver_history_downloads_series(ver)
-			save_to_hash(downloads,ver)
-		end
-		@hash
-	end
+    vers_list.each do |ver,built_at|
+      downloads = get_ver_history_downloads_series(ver)
+      save_to_hash(downloads,ver)
+    end
+    @hash
+  end
 
-	# call the method to get the updating downloads time ( yesterday )
-	def get_yesterday_downloads
-		vers_list = get_vers_list.keys
-		vers_list.each do |ver|
-			downloads = get_ver_yesterday_downloads(ver)
-			save_to_hash(downloads,ver)
-		end
-		@hash
-	end
+  # call the method to get the updating downloads time ( yesterday )
+  def get_yesterday_downloads
+    vers_list = get_vers_list.keys
+    vers_list.each do |ver|
+      downloads = get_ver_yesterday_downloads(ver)
+      save_to_hash(downloads,ver)
+    end
+    @hash
+  end
 
-	# related information
-	def get_info
-		gem_info = Gems.info @gem_name
-		gem_info
-	end
+  # related information
+  def get_info
+    gem_info = Gems.info @gem_name
+    gem_info
+  end
 
-	# historical dependencies
-	def get_dependencies
-		gem_dependencies = Gems.dependencies [@gem_name]
-		gem_dependencies
-	end
+  # historical dependencies
+  def get_dependencies
+    gem_dependencies = Gems.dependencies [@gem_name]
+    gem_dependencies
+  end
 
 end
--- lib/dailyminer.rb
+++ lib/dailyminer.rb
@@ -1,31 +1,37 @@
-require "./GemMiner.rb"
-require "ftools"
+require_relative "GemMiner"
 
-# read the gem list
-gem_names=File.open("gem_list.txt").readlines.each do |x| x.strip! end
+gem_map = ['retailigence', 'localizer', 'roopap', 'jekyll-minifier', 'vagrant-parallels', 'phidgets-ffi', 'dropbox-api']
 
-
-
-# all_dictionary=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
-
-
-# crawling the downloads accumulated and save them in unique file
-gem_names[1..10].each do |gem_name| 
-	puts gem_name 
-	begin
-		file=File.open("./downloads/"+gem_name+".txt","w")
-		gem_downloads = GemMiner.new gem_name
-		file.puts gem_downloads.get_versions_downloads_list
-	rescue
-		file=File.open("error_gems.txt","a")
-		file.puts gem_name
-	end
+gem_map = ['dropbox-api']
+def get_gem_downloads(gem_map)
+  downloads = gem_map.map do |gem_name|
+    rubygem = GemMiner.new(gem_name)
+    rubygem.get_info
+    yesterday = rubygem.get_yesterday_downloads
+    non_zero_dl = yesterday[gem_name].select { |ver, dls| dls.first[1] > 0 }
+    {gem_name => non_zero_dl}
+  end
 end
 
-
-# a=GemMiner.new "movie_crawler"
-# # puts a.get_vers_list
-# puts a.get_versions_downloads_list
-# # hash=a.get_versions
-# puts a.get_yesterday_downloads
-
+# require "ftools"
+#
+# # read the gem list
+# gem_names=File.open("gem_list.txt").readlines.each do |x| x.strip! end
+#
+#
+#
+# # all_dictionary=Hash.new{|h,k| h[k] = Hash.new(&h.default_proc)}
+#
+#
+# # crawling the downloads accumulated and save them in unique file
+# gem_names[1..10].each do |gem_name|
+#   puts gem_name
+#   begin
+#     file=File.open("./downloads/"+gem_name+".txt","w")
+#     gem_downloads = GemMiner.new gem_name
+#     file.puts gem_downloads.get_versions_downloads_list
+#   rescue
+#     file=File.open("error_gems.txt","a")
+#     file.puts gem_name
+#   end
+# end
